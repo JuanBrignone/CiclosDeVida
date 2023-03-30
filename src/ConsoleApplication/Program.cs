08c9682785b9ef30f64ede6cb0369b6adf0ac5ad
JuanBrignone <juan.brignone@correo.ucu.edu.uy>
2023-03-29 21:04:14 -0300

    Ciclos de Vida

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -19,9 +19,24 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
-            var train = new Train();
-            train.StartEngines();
-            Console.WriteLine("Hello World!");
+            //for (int i = 0; i <= 100000; i ++)
+            //{
+            //    Train train1 = new Train($"{i}");
+            //    Console.WriteLine(Train.Cont);
+            //}
+            //La memoria se llena antes de llegar a la cantidad de iteaciones del for, 
+            //por lo tanto no llega a completar el for y muestra en consola hasta el ultimo
+            // valor que llego a contar.
+        Train t1 = new Train ("Last Train To London");
+        Train t2 = new Train ("Last Train To London");
+        Train t3 = new Train ("Runaway Train");
+        t1 = t2;
+        t2 = t3;
+        //Al hacer t1 = t2 le estas asignando los atributos(id) de t1 a t2 y viceversa, por lo tanto
+        //t2 = "Last Train To London", pero al hacer t2 = t3, le estas asignando los valores de t3 a t2,
+        //por lo tanto t2 = "Runaway Train".
+        Console.WriteLine(t1.id + "\n" + t2.id + "\n" + t3.id);
+
         }
     }
 }
